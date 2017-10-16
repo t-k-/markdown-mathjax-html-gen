7d794756dd7bbdc133f3b3767182772c656c66d8
t-k- <clock126@126.com>
2017-10-16 11:08:02 -0400

    use mathjax CDN instead.

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
 	generate-md -layout jasonm23-foghorn --input ./input --output ./output
-	find output/ -name '*.html' | xargs sed -i 's%<head>%<head><script type="text/javascript" src="mathjax/MathJax.js?config=TeX-AMS-MML_SVG"></script>%'
+	find output/ -name '*.html' | xargs sed -i 's%<head>%<head><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-AMS-MML_SVG"></script>%'
 	sleep 2
 done
